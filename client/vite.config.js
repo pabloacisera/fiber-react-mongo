425ca01d68bbea9b19fef8f576ddf3220995461f
pabloacisera <pabloacisera2021@gmail.com>
2024-08-24 18:31:23 -0300

    sexto commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Asegúrate de que el directorio de salida sea 'dist'
+  },
 })
